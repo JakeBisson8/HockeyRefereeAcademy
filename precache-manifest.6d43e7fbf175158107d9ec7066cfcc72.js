1aae6e592a251e5f23ef625c584f3ca6c2a321bb
Jake Bisson <jbiss085@uottawa.ca>
2020-10-22 23:23:34 -0400

    Updates

---
+++ precache-manifest.6d43e7fbf175158107d9ec7066cfcc72.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd529130d20888a803d744decb2f1d83",
+    "url": "/HockeyRefereeAcademy/index.html"
+  },
+  {
+    "revision": "8c796488383c61dd46e6",
+    "url": "/HockeyRefereeAcademy/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8c796488383c61dd46e6",
+    "url": "/HockeyRefereeAcademy/static/js/2.c1a96329.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/HockeyRefereeAcademy/static/js/2.c1a96329.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "464bb611b4bdda1c4df7",
+    "url": "/HockeyRefereeAcademy/static/js/main.23a96633.chunk.js"
+  },
+  {
+    "revision": "26a1e73430b87d709779",
+    "url": "/HockeyRefereeAcademy/static/js/runtime-main.1255224e.js"
+  },
+  {
+    "revision": "6c274ab77a19e834fe11083a1b569ab2",
+    "url": "/HockeyRefereeAcademy/static/media/homepageimage.6c274ab7.jpg"
+  },
+  {
+    "revision": "d1df190fa526b22f1a8a7bf62f88add3",
+    "url": "/HockeyRefereeAcademy/static/media/mccauley.d1df190f.jpg"
+  },
+  {
+    "revision": "08fb222ea00b7b63b762f57f09faad66",
+    "url": "/HockeyRefereeAcademy/static/media/rooney.08fb222e.jpg"
+  },
+  {
+    "revision": "b6affb3a610da5af06ea87bd41a92368",
+    "url": "/HockeyRefereeAcademy/static/media/sutherland.b6affb3a.jpg"
+  }
+]);
